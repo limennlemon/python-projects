9f00aedf4308bfb520d53e42b3b8bd87ddac3d08
limennlemon <163668853+limennlemon@users.noreply.github.com>
2024-12-17 20:49:31 -0500

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+/*
+October 20, 2023
+Melanie Zwack
+CMSC 115, Prof. Rowson, Week 1: Project 1
+Write program that displays: Welcome to Java, Welcome to Computer Science, Programming is fun
+*/
+
+public class Welcome {
+    public static void main(String[] args) {
+        // Display message Welcome to Java on the console
+        System.out.println("Welcome to Java");
+        // Display message Welcome to Java on the console
+        System.out.println("Welcome to Computer Science");
+        // Display message Welcome to Java on the console
+        System.out.println("Programming is fun");
+    }
+}
